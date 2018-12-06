55a23ec03520660f787341f9fefc71fca5f92060
pogist <murilo.paixao.2@gmail.com>
2018-12-06 18:51:23 -0200

    feat: warns user when some animation keys are missing

--- lib/gosu/spritesheet.rb
+++ lib/gosu/spritesheet.rb
@@ -2,6 +2,9 @@ require "gosu/spritesheet/version"
 require "gosu/spritesheet/animation_block"
 
 module Gosu
+  class MissingAnimationKeys < StandardError; end
+  class UnknownAnimationKeys < StandardError; end
+
   class Spritesheet
     def initialize(tiles: [], animations: nil)
       @tiles = tiles
@@ -26,8 +29,17 @@ module Gosu
     end
 
     def animation_from_spec(spec)
-      frames_range = spec[:range]
-      duration = spec[:duration]
+      frames_range = spec.fetch(:range, nil)
+      duration = spec.fetch(:duration, nil)
+
+      missing_keys = []
+      missing_keys << ":range" if frames_range.nil?
+      missing_keys << ":duration" if duration.nil?
+
+      if not missing_keys.empty?
+        message = "Missing #{missing_keys.join(' and ')} on block: #{spec}" 
+        raise MissingAnimationKeys, message
+      end
 
       Gosu::AnimationBlock.new(@tiles[frames_range.first], duration)
     end
