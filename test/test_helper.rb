fdd120f82e18225525f4b3e4d827b3e80c50cfbf
pogist <murilo.paixao.2@gmail.com>
2018-12-06 14:46:34 -0200

    test: add reporters for minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,7 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "gosu/spritesheet"
 
 require "minitest/autorun"
+require "minitest/reporters"
+
+Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new(:color => true)]
+
